a34bbacaaf5e069e31bf2e826fe982cf66bf0575
N95JPL <37276225+N95JPL@users.noreply.github.com>
2020-06-18 19:31:16 +0100

    Button Box Script

---
+++ JoystickButton.ino
@@ -0,0 +1,61 @@
+// Simple example application that shows how to read four Arduino
+// digital pins and map them to the USB Joystick library.
+//
+// Ground digital pins 9, 10, 11, and 12 to press the joystick 
+// buttons 0, 1, 2, and 3.
+//
+// NOTE: This sketch file is for use with Arduino Leonardo and
+//       Arduino Micro only.
+//
+// by Matthew Heironimus
+// 2015-11-20
+//--------------------------------------------------------------------
+
+#include <Joystick.h>
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+  JOYSTICK_TYPE_GAMEPAD, 12, 0,
+  false, false, false, false, false, false,
+  false, false, false, false, false);;
+
+void setup() {
+  // Initialize Button Pins
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP); //Broken
+  pinMode(9, INPUT_PULLUP); //Broken
+  pinMode(10, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  
+  // Initialize Joystick Library
+
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 2;
+
+// Last state of the button
+int lastButtonState[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+int buttonPins[12] = {2,3,4,5,6,7,8,9,10,16,14,15};
+void loop() {
+
+  // Read pin values
+  for (int index = 0; index < 12; index++)
+  {
+    int currentButtonState = !digitalRead(buttonPins[index]);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
